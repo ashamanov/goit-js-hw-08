1a09ddac3d58a2a37de9af5110672032b0bf9e72
ashamanov <ashamanov.m@gmail.com>
2022-10-17 16:43:02 +0600

    Initial commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const ul = document.querySelector(".gallery");
+
+function createGallery(items) {
+  return items
+    .map(
+      (item) => `<a class="gallery__item" href="${item.original}">
+                    <img
+                      class="gallery__image"
+                      src=${item.preview}
+                      alt=${item.description}
+                    />
+                  </a>`
+    )
+    .join("");
+}
+
+const galleryMarkup = createGallery(galleryItems);
+ul.innerHTML = galleryMarkup;
+
+new SimpleLightbox(".gallery a", {
+    captions: true,
+    captionsData: 'alt',
+    captionDelay: 250,
+});
